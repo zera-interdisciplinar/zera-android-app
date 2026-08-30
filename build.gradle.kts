8335595b7bebe42f21b4526e65ff569037445885
aoas2009 <aoasilva122009@hotmail.com>
2026-08-30 17:52:29 -0300

    feat: conectando o projeto com o firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
