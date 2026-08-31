6139e0d748f80bb7326236e420376f15625e6042
aoas2009 <aoasilva122009@hotmail.com>
2026-08-30 23:34:37 -0300

    feat: Criando SplashScreen placeholder para configurar Navigation

---
+++ app/src/main/java/com/zera/android/view/screens/SplashScreen.kt
@@ -0,0 +1,15 @@
+package com.zera.android.view.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SplashScreen(){
+    Text(
+        text = "Hello World"
+    )
+}
