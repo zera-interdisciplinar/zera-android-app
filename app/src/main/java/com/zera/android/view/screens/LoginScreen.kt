06ceb99676acba6fde44c6ca1842c8b682d5f801
aoas2009 <aoasilva122009@hotmail.com>
2026-09-03 11:11:15 -0300

    feat: mudando layout das telas de Login e Welcome, com os novos inputs e sistema de botões

--- app/src/main/java/com/zera/android/view/screens/LoginScreen.kt
+++ app/src/main/java/com/zera/android/view/screens/LoginScreen.kt
@@ -9,12 +9,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.zera.android.view.components.buttons.ZeraButton
+import com.zera.android.view.components.buttons.ZeraButtonStyle
+import com.zera.android.view.components.buttons.ZeraButtonType
+import com.zera.android.view.components.inputs.ZeraInputType
 import com.zera.android.view.components.inputs.ZeraTextInput
 import com.zera.android.view.components.logo.Logo
 import com.zera.android.view.components.outros.SplashBackground
@@ -25,6 +33,9 @@ import com.zera.android.view.theme.ZeraTheme
 
 @Composable
 fun LoginScreen() {
+    var email by rememberSaveable { mutableStateOf("") }
+    var senha by rememberSaveable { mutableStateOf("") }
+
     SplashBackground()
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -40,13 +51,19 @@ fun LoginScreen() {
             ZeraTextInput(
                 label = "Email",
                 placeholder = "Seu email",
-                onValueChange = {},
+                value = email,
+                onValueChange = { email = it },
+                type = ZeraInputType.Email,
+                imeAction = ImeAction.Next,
                 modifier = Modifier.padding(vertical = Spacing.medium)
             )
             ZeraTextInput(
-                label = "Email",
-                placeholder = "Seu email",
-                onValueChange = {},
+                label = "Senha",
+                placeholder = "Senha aqui",
+                value = senha,
+                onValueChange = { senha = it },
+                type = ZeraInputType.Password,
+                imeAction = ImeAction.Done,
                 modifier = Modifier.padding(top = Spacing.medium)
             )
             CaptionText(
@@ -54,23 +71,20 @@ fun LoginScreen() {
                 underline = true,
                 onClick = {},
                 textAlign = TextAlign.Left,
-                modifier = Modifier.fillMaxWidth().padding(Spacing.micro),
+                modifier = Modifier.fillMaxWidth().padding(Spacing.small),
             )
             Spacer(Modifier.height(Spacing.xLarge))
-            ZeraButton(
-                text = "Entrar",
-                onClick = {
+            Column() {
+                ZeraButton(
+                    text = "Entrar",
 
-                }
-            )
-            CaptionText(
-                text = "Não tenho cadastro",
-                underline = true,
-                onClick = {
+                    onClick = {
 
-                }
-            )
-
+                    },
+                    modifier = Modifier.padding(Spacing.small),
+                    fillMaxWidth = true,
+                )
+            }
         }
     }
 }
