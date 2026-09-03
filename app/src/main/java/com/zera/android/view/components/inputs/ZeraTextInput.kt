33c708a54fdf7c4cfa62c9565505090436c01538
aoas2009 <aoasilva122009@hotmail.com>
2026-09-02 22:38:07 -0300

    feat: adicionando component ZeraTextInput

--- app/src/main/java/com/zera/android/view/components/inputs/ZeraTextInput.kt
+++ app/src/main/java/com/zera/android/view/components/inputs/ZeraTextInput.kt
@@ -1,18 +1,12 @@
 package com.zera.android.view.components.inputs
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
