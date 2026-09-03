b6d03892ccc8b7d0fb56fab5ae69fa71b081d04d
aoas2009 <aoasilva122009@hotmail.com>
2026-09-02 23:28:28 -0300

    feat: adicionando tela de boas vindas (WelcomeScreen.kt) e redirecionando da Splash pra ela

--- app/src/main/java/com/zera/android/view/screens/WelcomeScreen.kt
+++ app/src/main/java/com/zera/android/view/screens/WelcomeScreen.kt
@@ -1,13 +1,57 @@
 package com.zera.android.view.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.zera.android.view.components.buttons.ZeraButton
+import com.zera.android.view.components.buttons.ZeraButtonStyle
+import com.zera.android.view.components.logo.Logo
+import com.zera.android.view.components.outros.SplashBackground
+import com.zera.android.view.components.texts.LabelText
 import com.zera.android.view.components.texts.TitleText
 import com.zera.android.view.theme.ZeraTheme
 
 @Composable
 fun WelcomeScreen(){
-    TitleText("Hey it's me, identity crisity")
+    SplashBackground()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Logo(Modifier.width(250.dp).padding(bottom = 32.dp))
+            TitleText(text = "Bem vindo!", bold = true)
+            Spacer(Modifier.height(64.dp))
+            ZeraButton(
+                text = "Login",
+                style = ZeraButtonStyle.Yellow,
+                onClick = {
+
+                }
+            )
+            Spacer(Modifier.height(16.dp))
+            LabelText(
+                text = "Primeiro Acesso",
+                underline = true,
+                bold = true,
+                onClick = {
+
+                }
+            )
+        }
+    }
+
 }
 
 @Composable
