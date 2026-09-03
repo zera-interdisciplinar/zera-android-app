84e3cb83d82f7ec579d0e611f4eabb1588a0c6ab
aoas2009 <aoasilva122009@hotmail.com>
2026-09-03 00:20:40 -0300

    fix: arrumando bug de cores no botão

--- app/src/main/java/com/zera/android/view/components/inputs/ZeraTextInput.kt
+++ app/src/main/java/com/zera/android/view/components/inputs/ZeraTextInput.kt
@@ -2,16 +2,21 @@ package com.zera.android.view.components.inputs
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import com.zera.android.view.components.texts.BodyText
 import com.zera.android.view.components.texts.LabelText
 import com.zera.android.view.theme.Radius
+import com.zera.android.view.theme.Spacing
 import com.zera.android.view.theme.ZeraTheme
 
 @Composable
@@ -20,10 +25,15 @@ fun ZeraTextInput(
     value: String = "",
     onValueChange: (String) -> Unit,
     label: String = "",
-    placeholder: String = ""
+    placeholder: String = "",
+    width: Dp? = null,
 ){
-    Column() {
-        LabelText(label)
+    Column(
+        modifier = modifier.then(
+            if (width != null) Modifier.width(width) else Modifier.fillMaxWidth()
+        )
+    ) {
+        LabelText(label, Modifier.padding(Spacing.small))
         TextField(
             value = value,
             onValueChange = onValueChange,
@@ -35,11 +45,14 @@ fun ZeraTextInput(
             colors = TextFieldDefaults.colors(
                 focusedContainerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
                 unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
-                focusedIndicatorColor = MaterialTheme.colorScheme.primary,
-                unfocusedIndicatorColor = MaterialTheme.colorScheme.outline,
-                disabledIndicatorColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.38f)
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                errorIndicatorColor = Color.Transparent,
             ),
-            modifier = modifier.fillMaxWidth()
+            modifier = Modifier.then(
+                if (width != null) Modifier.width(width) else Modifier.fillMaxWidth()
+            )
         )
     }
 }
