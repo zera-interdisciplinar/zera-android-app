828ba78e7a7b30cc36c9a1a245c476ab3b79cfcc
aoas2009 <aoasilva122009@hotmail.com>
2026-09-02 23:28:28 -0300

    feat: adicionando tela de boas vindas (WelcomeScreen.kt) e redirecionando da Splash pra ela

--- app/src/main/java/com/zera/android/view/screens/SplashScreen.kt
+++ app/src/main/java/com/zera/android/view/screens/SplashScreen.kt
@@ -1,39 +1,38 @@
 package com.zera.android.view.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.zera.android.view.components.logo.Logo
 import com.zera.android.view.components.progressbars.ProgressBar
 import com.zera.android.view.components.outros.SplashBackground
-import com.zera.android.view.components.texts.BodyText
 import com.zera.android.view.components.texts.SubtitleText
-import com.zera.android.view.components.texts.TitleText
-import com.zera.android.view.theme.Typography
+import com.zera.android.view.navigation.Route
+import com.zera.android.view.navigation.ZeraNavigator
 import com.zera.android.view.theme.ZeraTheme
+import kotlinx.coroutines.Delay
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen() {
+    LaunchedEffect(Unit) {
+        delay(2000)
+        ZeraNavigator.pushAndPop(Route.Welcome)
+    }
+    SplashBackground()
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.surface),
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        SplashBackground(modifier = Modifier.fillMaxSize())
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
