a2c3de8f61187d3800407d32188fcfb21fa20f19
aoas2009 <aoasilva122009@hotmail.com>
2026-09-02 23:27:09 -0300

    refactor: trocando o Text de botão pelos textos padronizados, e adicionando um pequeno padding horizontal

--- app/src/main/java/com/zera/android/view/components/buttons/ZeraButton.kt
+++ app/src/main/java/com/zera/android/view/components/buttons/ZeraButton.kt
@@ -3,11 +3,11 @@ package com.zera.android.view.components.buttons
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.zera.android.view.components.texts.BodyText
 import com.zera.android.view.theme.ZeraTheme
 
 /**
@@ -37,7 +37,7 @@ fun ZeraButton(
         enabled = enabled,
         colors = buttonColors(style, type),
     ) {
-        Text(text = text)
+        BodyText(text = text, bold = true, modifier = Modifier.padding(horizontal = 16.dp))
     }
 }
 
