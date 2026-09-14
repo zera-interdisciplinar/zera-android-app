036cab78c3bacf217fef81c735b00eb57c0605ff
aoas2009 <aoasilva122009@hotmail.com>
2026-09-13 23:08:18 -0300

    feat: criando novo metodo de navegacao PushAndPopAll

--- app/src/main/java/com/zera/android/view/navigation/ZeraNavigator.kt
+++ app/src/main/java/com/zera/android/view/navigation/ZeraNavigator.kt
@@ -16,11 +16,11 @@ object ZeraNavigator {
         _commands.trySend(NavCommand.GoBack)
     }
 
-    /**
-     * Navega para [route] e remove a tela atual da pilha, impedindo que o usuário
-     * volte para ela. Ver [NavCommand.PushAndPop].
-     */
     fun pushAndPop(route: Route){
         _commands.trySend(NavCommand.PushAndPop(route))
+    }
+
+    fun pushAndPopAll(route: Route){
+        _commands.trySend(NavCommand.PushAndPopAll(route))
     }
 }
